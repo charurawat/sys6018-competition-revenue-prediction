5c667bba89f16d704d81192f7cd7d9083f7cac4c
Beni Shpringer <bshpringer@gmail.com>
2018-10-24 09:35:22 -0400

    add code to load in data from json columns

--- initial_regression.py
+++ initial_regression.py
@@ -1,12 +1,32 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import pandas as pd
-import numpy as np
 import os
-os.chdir('/Users/Beni/Desktop/Fall_UVA/SYS6018/Competitions/comp_4/sys6018-competition-revenue-prediction')
+import json
+import numpy as np
+import pandas as pd
+from pandas.io.json import json_normalize
 
-n_obs = 100
+def load_df(csv_path='./all/train.csv', nrows=None):
+    JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
+    
+    df = pd.read_csv(csv_path, 
+                     converters={column: json.loads for column in JSON_COLUMNS}, 
+                     dtype={'fullVisitorId': 'str'}, 
+                     nrows=nrows)
+    
+    for column in JSON_COLUMNS:
+        column_as_df = json_normalize(df[column])
+        column_as_df.columns = [f"{column}.{subcolumn}" for subcolumn in column_as_df.columns]
+        df = df.drop(column, axis=1).merge(column_as_df, right_index=True, left_index=True)
+    print(f"Loaded {os.path.basename(csv_path)}. Shape: {df.shape}")
+    return df
 
-test_data = pd.read_csv('./all/train.csv', nrows = n_obs)
-test_data
+n_obs = 1000
+
+df_train_small = load_df(nrows=n_obs)
+df_train_small
+
+df_test_small = load_df(csv_path='./all/test.csv', nrows = n_obs)
+
+    
