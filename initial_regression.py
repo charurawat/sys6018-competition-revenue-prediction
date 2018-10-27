827b6bfd3ea72064edfb618b9a37c5c028f3eb2b
Beni Shpringer <bshpringer@gmail.com>
2018-10-27 11:55:53 -0400

    change hits and pageviews to numeric, group on user id and country

--- initial_regression.py
+++ initial_regression.py
@@ -1,11 +1,16 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
 
-import os
-import json
+#import os
+#import json
 import numpy as np
 import pandas as pd
-from pandas.io.json import json_normalize
+from sklearn.linear_model import LogisticRegression, LogisticRegressionCV
+#from pandas.io.json import json_normalize
+#from sklearn.linear_model import Ridge, RidgeCV, Lasso, LassoCV
+#from sklearn.model_selection import KFold
+#import math
+
 
 def load_df(csv_path='./all/train.csv', nrows=None):
     JSON_COLUMNS = ['device', 'geoNetwork', 'totals', 'trafficSource']
@@ -14,11 +19,22 @@ def load_df(csv_path='./all/train.csv', nrows=None):
         df = df.join(pd.DataFrame(df.pop(column).apply(pd.io.json.loads).values.tolist(), index=df.index))
     return df
 
-n_obs = 1000
+n_obs = 50000
 
-df_train_small = load_df(nrows=n_obs)
-df_train_small
+df_train = load_df(nrows=n_obs)
+#df_test = load_df(csv_path='./all/test.csv', nrows = n_obs)
 
-df_test_small = load_df(csv_path='./all/test.csv', nrows = n_obs)
+df_train.transactionRevenue = [float(x) if (float(x) > 0) else 0 for x in df_train.transactionRevenue]
 
-    
+df_train[['hits','pageviews']] = df_train[['hits','pageviews']].apply(pd.to_numeric)
+
+
+by_user_id = df_train.groupby(['fullVisitorId', 'country'])[['hits', 'pageviews', "transactionRevenue"]].sum().reset_index().rename_axis(None, axis=1)
+
+by_user_id.transactionRevenue = [np.log(x) if x > 0 else 0 for x in by_user_id.transactionRevenue]
+
+#x_values = pd.concat([df_train.hits, df_train.pageviews], axis = 1)
+
+
+
+
